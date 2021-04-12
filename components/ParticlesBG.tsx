bec6f3d077d65781100524496921144fb65590ff
nullhund <0x1f415@users.noreply.github.com>
2021-04-11 20:54:57 -0400

    tewak particle parameters

--- components/ParticlesBG.tsx
+++ components/ParticlesBG.tsx
@@ -1,52 +1,52 @@
-import React from "react";
-import Particles from "react-particles-js";
+import React from 'react';
+import Particles from 'react-particles-js';
 
-import styles from "./ParticlesBG.module.scss";
+import styles from './ParticlesBG.module.scss';
 
 export default function ParticlesBG() {
-  return (
-    <Particles
-      params={{
-        move: {
-          speed: 1,
-          out_mode: "out",
-        },
-        particles: {
-          line_linked: {
-            enable: false,
-          },
-          shape: {
-            type: "image",
-            image: [
-              {
-                src: "/paw.png",
-                height: 20,
-                width: 20,
-              },
-              {
-                src: "/peach.png",
-                height: 20,
-                width: 20,
-              },
-            ],
-          },
-          color: {
-            value: "#CCC",
-          },
-          size: {
-            value: 30,
-            random: true,
-            anim: {
-              enable: true,
-              speed: 4,
-              size_min: 10,
-              sync: false,
-            },
-          },
-        },
-        retina_detect: false,
-      }}
-      className={styles.ParticlesBG}
-    />
-  );
+	return (
+		<Particles
+			params={{
+				move: {
+					speed: 1,
+					out_mode: 'out'
+				},
+				particles: {
+					line_linked: {
+						enable: false
+					},
+					shape: {
+						type: 'image',
+						image: [
+							{
+								src: '/paw.png',
+								height: 20,
+								width: 20
+							},
+							{
+								src: '/peach.png',
+								height: 20,
+								width: 20
+							}
+						]
+					},
+					color: {
+						value: '#CCC'
+					},
+					size: {
+						value: 15,
+						random: false
+					},
+					opacity: {
+						value: 0.5,
+						anim: {
+							enable: false
+						}
+					}
+				},
+				retina_detect: false
+			}}
+			className={styles.ParticlesBG}
+		/>
+	);
 }
